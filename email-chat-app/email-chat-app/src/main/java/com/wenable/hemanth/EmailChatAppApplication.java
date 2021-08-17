dbecf16ae862f52fdf783b1a0d23c4d3f7a113e2
Hemanth7207 <89058796+Hemanth7207@users.noreply.github.com>
2021-08-17 14:54:05 +0530

    first commit

---
+++ email-chat-app/email-chat-app/src/main/java/com/wenable/hemanth/EmailChatAppApplication.java
@@ -0,0 +1,15 @@
+package com.wenable.hemanth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class EmailChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailChatAppApplication.class, args);
+	}
+
+}
